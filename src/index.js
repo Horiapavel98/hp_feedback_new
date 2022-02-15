b3b1436d27fee5eb4071eed29cb0ebf96edd6c37
Horia <32480920+Horiapavel98@users.noreply.github.com>
2022-02-15 11:17:30 +0000

    Add initial form functionality

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";
+import "./styles.css";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const FeedbackForm = () => {
+  const db = firebase.firestore();
+
+  const formik = useFormik({
+    initialValues: { feedback: "" },
+    onSubmit: (values) => {
+      // Handle errors in connection
+      db.collection("feedback").add(values);
+      alert("Feedback submitted successfully!");
+    },
+  });
+  return (
+    // <LimitedTextarea limit={30} value="Hello!"/>
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="feedback">Write your feedback for Horia here ...</label>
+      <br />
+      <textarea
+        id="feedback"
+        name="feedback"
+        type="text"
+        rows={10}
+        cols={300}
+        onChange={formik.handleChange}
+        value={formik.values.feedback}
+      />
+      <br />
+      {/* <input
+        id="feedback"
+        name="feedback"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.feedback}
+      /> */}
+      <button type="submit">Send feedback</button>
+    </form>
+  );
+};
+
+function App() {
+  return <FeedbackForm />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
